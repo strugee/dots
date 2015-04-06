86077ebe8738e3ea712ce3f5d735d13c8fd0e017
Alex Jordan <alex@strugee.net>
2015-04-06 12:41:25 -0700

    Add basic Gnus configuration

---
+++ .gnus.el
@@ -0,0 +1,8 @@
+;;;; Gnus startup
+; Alex Jordan
+; 05 April 2015
+
+(setq user-mail-address "alex@strugee.net")
+(setq user-full-name "Alex Jordan")
+(setq gnus-select-method '(nnnil ""))
+(setq mail-sources '((maildir)))
